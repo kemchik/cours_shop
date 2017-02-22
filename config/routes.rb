3ccd9a14178a4031a32fe26c9a901d0b3be6d81f
kemchik <kerminsk@gmail.com>
2017-02-22 12:08:23 +0300

    add devise

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+
+  root to: "home#index"  #home controller, index action (method)
+
+  devise_for :users
+  get 'home/register'
+  get 'persons/profile'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
